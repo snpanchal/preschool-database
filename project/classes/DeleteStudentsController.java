c5d28c2b0ca3075ad3518df8cc7a7cf63588e2e7
snpanchal <snpanchal44@gmail.com>
2017-09-17 16:49:57 -0400

    Update and rename DeleteStudentsController.java to project/classes/DeleteStudentsController.java

--- project/classes/DeleteStudentsController.java
+++ project/classes/DeleteStudentsController.java
@@ -1,10 +1,13 @@
-package preschool;
-
 import java.sql.SQLException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+/**
+* @author Shyam Panchal
+* Copyright Little People's Pre-School 2017
+*/
 
 public class DeleteStudentsController extends SwitchScenes {
 	@FXML private TextField studentField;
