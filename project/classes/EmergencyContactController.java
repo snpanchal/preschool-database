a796921e4cbf508808355c3f9962133cbbfbb900
snpanchal <snpanchal44@gmail.com>
2017-09-17 16:51:51 -0400

    Update and rename EmergencyContactController.java to project/classes/EmergencyContactController.java

--- project/classes/EmergencyContactController.java
+++ project/classes/EmergencyContactController.java
@@ -1,11 +1,14 @@
-package preschool;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+/**
+ * @author Shyam Panchal
+ * Copyright Little People's Pre-School 2017
+*/
 
 public class EmergencyContactController extends SwitchScenes {
 	@FXML private TextField studentSearch;
